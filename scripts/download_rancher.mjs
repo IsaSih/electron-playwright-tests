d5f3eabf8fb4e252e23876f5f111ab47a15ce807
IsaSih <isabela.sguima@gmail.com>
2022-08-26 10:03:21 -0700

    rd and get rdctl version

--- scripts/download_rancher.mjs
+++ scripts/download_rancher.mjs
@@ -2,10 +2,11 @@ import path from 'path';
 import os from 'os';
 import fs from 'fs';
 import fetch from 'node-fetch';
-import { execFile, execFileSync } from 'child_process';
-//import { stdout } from 'process';
+import { execFileSync, execFile } from 'child_process';
 import util from 'util';
 import { download } from './lib/download.mjs';
+import pkg from 'webpack';
+import { exit } from 'process';
 
 //step 1: download latest build of Rancher Desktop
 const response = await fetch('https://rancherdesktop.io/');
@@ -28,9 +29,12 @@ console.log(destPath);
 //step 2: install Rancher Desktop
 let stdout = 'string';
 let volume = '';
+const appPath = '/Applications/Rancher Desktop.app';
 
 try{
    await download(installMacLink, destPath, { overwrite: true });
+
+   execFileSync('bash', ['-c', `rm -rf '${appPath}/'`]);
 
   stdout = await execFileSync(
     'bash',
@@ -40,27 +44,22 @@ try{
   volume = stdout.toString().trim();
   console.log(volume);
 
+  //execFileSync('bash', ['-c', `rm -rf '${appPath}/'`]);
+  execFileSync('bash', ['-c', `cp -r '${volume}/Rancher Desktop.app' /Applications/`]);
 
-  //await execFile('cp', ['-rf', `${volume}.app`, '/Applications']);
-  //const appPath = path.join(`${volume}.app`);
-  //console.log(appPath);
+execFileSync('bash', [ '-c', `hdiutil detach '${volume}'`], { encoding: 'utf-8' });
 
-  //const rdVersion = new RegExp('Rancher Desktop [0-9.]+');
-  //const matcher = volume.match(rdVersion);
-  //console.log(matcher[0]);
+execFileSync('bash', ['-c', `xattr -d -r com.apple.quarantine '${appPath}'`], { encoding: 'utf-8' });
+//await util.promisify(setTimeout(() => {  execFileSync('bash', ['-c', `xattr -d -r com.apple.quarantine '${appPath}'`], { encoding: 'utf-8' }); }, 70000));
 
-  fs.rmSync(`/Applications/Rancher Desktop.app`, {
-    force: true,
-    recursive: true,
-  });
-  const appPath = '/Applications/Rancher Desktop.app';
-  await execFile('bash', [ '-c', `cp -r '${volume}/Rancher Desktop.app' /Applications/`]);
-  await execFile('bash', [ '-c', `cp -r '${volume}/Rancher Desktop.app' /Applications/`]);
-  await execFile('bash', ['-c', `xattr -d -r com.apple.quarantine ${appPath}`]);
-  fs.rmSync(volume, { recursive: true });
+fs.rmSync(volume, { recursive: true, force: true });
+
   try {
-      fs.promises.access(`${appPath}`, fs.constants.X_OK, fs.constants.R_OK)
+    fs.promises.access(`${appPath}`, fs.constants.X_OK, fs.constants.R_OK);
         console.log('Rancher Desktop sucessfully installed!');
+        // step 3: Run Rancher Desktop
+        execFileSync('bash', [ '-c',`open '${appPath}'`], { encoding: 'utf-8' });
+        console.log(execFileSync('bash', [ '-c','rdctl version'], { encoding: 'utf-8' }));
     }
     catch (error) {
       throw error;
@@ -73,12 +72,3 @@ console.log('Error: ', e);
  fs.rmSync(workDir, { recursive: true });
 
 }
-//step 3: Run Rancher Desktop
-
-
-//Get rdctl version
-
-  //const output = exec('rdctl', ['version']);
-  //console.log(output.stdout);
-
- //fs.rmSync(appPath, {recursive:true});
